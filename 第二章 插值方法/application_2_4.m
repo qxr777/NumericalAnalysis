b3dd2110d79f82663292392b2874e3cb2ad2b088
qxr777 <qixin_y20005@163.com>
2020-03-14 10:54:03 +0800

    新增插值法实现数学函数应用实例

---
+++ 第二章 插值方法/application_2_4.m
@@ -0,0 +1,10 @@
+% 利用插值法实现数学函数
+clear; clc;
+x = 0 : 1 : 5;
+fprintf('sin函数值    my_sin函数值   误差\n');
+for k = 1 : length(x)
+    y = sin(x(k));
+    y_star = my_sin(x(k));
+    error = abs(y - y_star);    
+    fprintf('%.10d       %.10d      %.10d\n', y, y_star, error);
+end
