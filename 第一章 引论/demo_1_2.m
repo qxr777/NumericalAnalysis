145e47521459defdcfd6a929702651abe29ba6de
Xin Qi <qxr777@whut.edu.cn>
2022-09-05 13:36:01 +0800

    Fix bug

--- 第一章 引论/demo_1_2.m
+++ 第一章 引论/demo_1_2.m
@@ -23,8 +23,8 @@ disp(S)
 % 精确值
 syms x
 for n = 1 : 5 % 8
-    St(n) = int(x^n/(x+5),0,1);
-    St(n) = vpa(St(n),3);
+    S(n) = int(x^n/(x+5),0,1);
+    S(n) = vpa(S(n),3);
 end
 disp('精确值：')
 disp(S)
